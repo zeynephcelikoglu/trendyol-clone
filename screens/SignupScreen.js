d5be707f54df02d23d6fa3827b3c28f1be736eaa
Zeynep Hanife Çelikoğlu <zeyneph807@gmail.com>
2025-04-04 15:42:19 +0300

    feat(signup): connect signup screen to Firebase and AuthContext

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,11 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Alert } from 'react-native'
+import React, { useState, useContext } from 'react'
 import AuthContent from '../components/AuthContent'
+import { createUser } from '../util/auth'
+import Loading from '../components/Loading'
+import { AuthContext } from '../store/auth-context'
 
 export default function SignupScreen() {
-  return (
-    <AuthContent/>
-  )
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+  const authContext = useContext(AuthContext)
+
+  async function signUpHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      const token = await createUser(email, password);
+      authContext.authenticate(token);
+    } catch (error) {
+      Alert.alert("Kayıt Başarısız", error.message);
+    } finally {
+      setIsAuthenticating(false);
+    }
+  }
+
+  if (isAuthenticating) {
+    return <Loading message="Kullanıcı oluşturuluyor..." />
+  }
+
+  return <AuthContent onAuthenticate={signUpHandler} />
 }
 
 const styles = StyleSheet.create({})
