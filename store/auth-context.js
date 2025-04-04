6ccbd7e31e85e2cccd6d1c3f9c1aa0650b29bdfa
Zeynep Hanife Çelikoğlu <zeyneph807@gmail.com>
2025-04-04 15:41:18 +0300

    feat(auth): add AuthContext with token persistence using AsyncStorage

---
+++ store/auth-context.js
@@ -0,0 +1,45 @@
+import { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const AuthContext = createContext ({
+    token: "",
+    isAuthenticated: false,
+    authenticate: (token)=> {},
+    logout:()=> {}
+})
+
+    function AuthContextProvider({children}) {
+        const [authToken, setAuthToken] = useState(null)
+
+        useEffect(()=>{
+
+            async function fetchToken() {
+                const storedToken = await AsyncStorage.getItem("token")
+
+                if(storedToken) {
+                    setAuthToken(storedToken)
+                }
+            }
+
+            fetchToken()
+        },[])
+        
+        function authenticate(token){
+            setAuthToken(token)
+            AsyncStorage.setItem("token", token)
+        }
+        function logout(token){
+            setAuthToken(null)
+        }
+
+        const value = {
+            token: authToken,
+            isAuthenticated: !!authToken,
+            authenticate: authenticate,
+            logout: logout
+        }
+
+        return <AuthContext.Provider value={value} >{children}</AuthContext.Provider>
+    }
+
+export default AuthContextProvider;
