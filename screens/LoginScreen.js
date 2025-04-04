ac2d871e30e8851f050b3c5da3bdd8f848a73093
Zeynep Hanife Çelikoğlu <zeyneph807@gmail.com>
2025-04-04 15:46:51 +0300

    feat(login): implement login flow with loading, error handling and context auth

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Alert } from 'react-native'
+import React, {useState, useContext} from 'react'
 import AuthContent from '../components/AuthContent'
+import Loading from '../components/Loading'
+import { login } from '../util/auth'
+import { AuthContext } from '../store/auth-context'
 
 export default function LoginScreen() {
-  return (
-      <AuthContent isLogin />
-  )
+  const [isAuthanticating, setIsAuthanticating] = useState(false)
+  const authContext= useContext(AuthContext)
+  
+  async function loginHandler({ email, password }) {
+    setIsAuthanticating(true);
+    try {
+      const token = await login(email, password);
+      authContext.authenticate(token)
+    } catch (error) {
+      Alert.alert("Giriş Başarısız", error.response?.data?.error?.message || "Lütfen bilgilerinizi kontrol ediniz"
+      );
+      console.error(error);
+  } finally {
+      setIsAuthanticating(false);
+    }
+  }
+  
+    if (isAuthanticating) {
+      return <Loading message="Giriş yapılıyor..." />
+    }
+
+  return <AuthContent onAuthenticate={loginHandler} isLogin={true}/>
 }
 
 const styles = StyleSheet.create({})
